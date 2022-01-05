cef44bd5bdabe519ef524ac39f6f7540e320091a
Vitor Mateus <vitor7jan@gmail.com>
2022-01-05 17:03:38 -0300

    inclusão views conta

--- Originarios/Originarios/Scripts/Script.js
+++ Originarios/Originarios/Scripts/Script.js
@@ -1,16 +1,11 @@
 ﻿/********************* Formatação máscara data nascimento *********************/
 const formatDtNasc = () => {
-    const dtNasc = document.getElementById('dtNasc').innerHTML.trim();
-    if (dtNasc) {
-        document.getElementById('dtNasc').innerHTML = dtNasc.substring(0, 10);
-        return;
-    }
     const hiddenDtNasc = document.getElementById('hiddenDtNasc').value.trim();
     if (hiddenDtNasc) {
         const day = hiddenDtNasc.substring(0, 2);
         const month = hiddenDtNasc.substring(3, 5);
         const year = hiddenDtNasc.substring(6, 10);
-        document.getElementById('dtNasc').value = `${year}-${month}-${day}`;
+        document.getElementById('inputDate').value = `${year}-${month}-${day}`;
     }
 }
 window.addEventListener('load', formatDtNasc);
